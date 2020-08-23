a1df43435199ff21f56c99edb9bb9b77261da151
Den <dionisvl3@gmail.com>
2020-08-23 16:44:05 +0300

    вынес в конструктор Db::getLink()

--- App/Model/Task.php
+++ App/Model/Task.php
@@ -7,26 +7,30 @@ use Exception;
 
 class Task
 {
+    private $link;
+
+    public function __construct()
+    {
+        $this->link = Db::getLink();
+    }
+
     public function create(string $name, string $email, string $text, string $status)
     {
         $query = 'INSERT INTO tasks(name, email, text, status) VALUES(?,?,?,?)';
 
-        $link = Db::getLink();
-        $stmt = $link->prepare($query);
+        $stmt = $this->link->prepare($query);
 
         $stmt->bind_param('ssss', $name, $email, $text, $status);
 
         if ($stmt->execute()) {
-            return $link->insert_id;
+            return $this->link->insert_id;
         } else {
-            throw new Exception($link->error);
+            throw new Exception($this->link->error);
         }
     }
 
     public function getAll(string $orderBy, string $orderDirection, int $perPage = 3, int $page = 1)
     {
-        $link = Db::getLink();
-
         $startRow = ($page - 1) * $perPage;
 
         $query = "
@@ -36,26 +40,22 @@ ORDER BY
     $orderBy $orderDirection
 LIMIT $perPage
 OFFSET $startRow";
-//dump($query);
-        $result = mysqli_query($link, $query);
+
+        $result = mysqli_query($this->link, $query);
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
     public function getCount(): int
     {
-        $link = Db::getLink();
-
-        $result = $link->query("SELECT COUNT(*) FROM `tasks`");
+        $result = $this->link->query("SELECT COUNT(*) FROM `tasks`");
         $row = $result->fetch_row();
         return $row[0];
     }
 
     public static function getById(int $id)
     {
-        $link = Db::getLink();
-
         $query = "SELECT * FROM tasks WHERE `id` = '$id'";
-        $result = mysqli_query($link, $query);
+        $result = mysqli_query(Db::getLink(), $query);
         return mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
     }
 
@@ -63,8 +63,7 @@ OFFSET $startRow";
     {
         $query = 'UPDATE tasks SET `name`=?, `email`=?,`text`=?,`status`=?,`edited`=? WHERE id=?';
 
-        $link = Db::getLink();
-        $stmt = $link->prepare($query);
+        $stmt = $this->link->prepare($query);
 
         $edited = (int)$edited;
         $stmt->bind_param('ssssii', $name, $email, $text, $status, $edited, $id);
--- App/Model/User.php
+++ App/Model/User.php
@@ -8,35 +8,38 @@ use Exception;
 
 class User
 {
+    private $link;
+
+    public function __construct()
+    {
+        $this->link = Db::getLink();
+    }
+
     public function create(string $name, string $password, string $email)
     {
         $query = 'INSERT INTO users(name, password, email) VALUES(?,?,?)';
 
-        $link = Db::getLink();
-        $stmt = $link->prepare($query);
+        $stmt = $this->link->prepare($query);
         $stmt->bind_param('sss', $name, $password, $email);
 
         if ($stmt->execute()) {
-            return $link->insert_id;
+            return $this->link->insert_id;
         } else {
-            throw new Exception($link->error);
+            throw new Exception($this->link->error);
         }
     }
 
     public static function getById(int $id)
     {
-        $link = Db::getLink();
-
         $query = "SELECT * FROM users WHERE `id` = '$id'";
-        $result = mysqli_query($link, $query);
+        $result = mysqli_query(Db::getLink(), $query);
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
     public static function getByName(string $name)
     {
-        $link = Db::getLink();
-
         $sql = "SELECT * FROM users WHERE name=?"; // SQL with parameters
+        $link = Db::getLink();
         $stmt = $link->prepare($sql);
         $stmt->bind_param("s", $name);
 
@@ -45,35 +48,5 @@ class User
         while ($row = $result->fetch_assoc()) {
             return $row;
         }
-
     }
-
-    //проверка данных авторизации
-//    function ValidData($login, $pass)
-//    {
-//        $sql = "SELECT * FROM users WHERE login='$login' and pass='$pass'";
-//        $result = mysql_query($sql) or die(mysql_error());
-//        if (mysql_num_rows($result)) {
-//            $_SESSION["Auth"] = true;
-//            $_SESSION["User"] = $login;
-//        } else $_SESSION["Auth"] = false;
-//
-//        if (!$_SESSION["Auth"]) {
-//            $msg = "<em><span style='color:red'>Данные введены не верно!</span></em>";
-//        } else {
-//            $msg = "<em><span style='color:green'>Вы верно ввели данные!</span></em>";
-//            $unVisibleForm = true;
-//        }
-//
-//        $result = array("unVisibleForm" => $unVisibleForm,
-//            "userName" => $login,
-//            "msg" => $msg,
-//            "login" => $login,
-//            "pass" => $pass,);
-//        return $result;
-//    }
 }
-
-
-?>  
-  
